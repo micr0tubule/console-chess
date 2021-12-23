ce348c2029a5ee397417795f7dc2d28b46312217
micr0tubule <63601425+micr0tubule@users.noreply.github.com>
2021-12-23 19:58:05 +0100

    Update game_state.py

--- game_state.py
+++ game_state.py
@@ -18,16 +18,10 @@ class GameState:
 
 
     def get_non_current_party(self): 
-        '''
-        returns the party that is not the current party
-        '''
         return self.white_party if self.current_party is self.red_party else self.red_party 
     
 
     def apply(self): 
-        '''
-        apply changes to board 
-        '''
         self.board = numpy.zeros((8, 8, 2))
         for piece in self.red_party.pieces + self.white_party.pieces:
             self.board[piece.position[0], piece.position[1]][0] = int(piece) 
